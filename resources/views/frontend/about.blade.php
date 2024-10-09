6cc32bb4a5b19d0caad1bcf022ccf7a6ae3bd675
Ctribsz <122636369+Ctribsz@users.noreply.github.com>
2024-10-08 20:18:46 -0600

    Github Actions prueba

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1,168 +1,47 @@
 @extends('templates.frontend')
 
 @section('content')
-<section class="ecommerce-about">
-        <div class="effect d-none d-md-block">
-            <div class="ecommerce-effect bg-primary"></div>
-            <div class="ecommerce-effect bg-info"></div>
-        </div>
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6">
-                    <h1 class="fw-bold mb-3">About Us</h1>
-                    <p class="fs-16 text-muted mb-5 mb-lg-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                </div>
-                <div class="col-lg-6">
-                    <div>
-                        <div class="row align-items-center">
-                            <div class="col-md-6">
-                                <div class="position-relative">
-                                    <img src="front/images/ecommerce/img-4.jpg" alt="" class="img-fluid rounded">
+<div class="w-100">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-6">
+                            <div class="auth-card mx-lg-3">
+                                <div class="card border-0 mb-0">
+                                    <div class="card-body text-center p-4">
+
+                                        <div class="text-center px-sm-5 mx-5">
+                                            <img src="../assets/images/error500.png" class="img-fluid" alt="">
+                                        </div>
+                                        <div class="mt-4 text-center pt-3">
+                                            <div class="position-relative">
+                                                <h4 class="fs-18 error-subtitle text-uppercase mb-0">Internal Server Error</h4>
+                                                <p class="fs-15 text-muted mt-3">It will be as simple as Occidental in fact, it will Occidental to an English person</p>
+                                                <div class="mt-4">
+                                                    <a href="index.html" class="btn btn-primary"><i class="mdi mdi-home me-1"></i>Back to home</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="vstack gap-4">
-                                    <img src="front/images/ecommerce/img-1.jpg" alt="" class="img-fluid rounded">
-                                    <img src="front/images/ecommerce/img-3.jpg" alt="" class="img-fluid rounded">
+                        </div><!--end col-->
+                    </div><!--end row-->
+                </div><!--end container-->
+
+                <footer class="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <div class="text-center">
+                                    <p class="mb-0 text-muted">©
+                                        <script>document.write(new Date().getFullYear())</script> Toner. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand
+                                    </p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </footer>
             </div>
-        </div>
-    </section>
-
-    <div class="ecommerce-about-cta">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="card card-animate border-0">
-                        <div class="card-body">
-                            <lord-icon src="https://cdn.lordicon.com/fcoczpqi.json" trigger="hover" target="div" style="width:70px;height:70px">
-                            </lord-icon>
-                            <h5 class="fs-16 mt-3">MISION</h5>
-                            <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                            <div>
-                                <a href="#!" class="link-effect link-primary">Read More <i class="bi bi-arrow-right ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="card card-animate border-0">
-                        <div class="card-body">
-                            <lord-icon src="https://cdn.lordicon.com/hbwqfgcf.json" trigger="hover" target="div" style="width:70px;height:70px">
-                            </lord-icon>
-                            <h5 class="fs-16 mt-3">VISION</h5>
-                            <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                            <div>
-                                <a href="#!" class="link-effect link-primary">Read More <i class="bi bi-arrow-right ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="card card-animate border-0">
-                        <div class="card-body">
-                            <lord-icon src="https://cdn.lordicon.com/xhbsnkyp.json" trigger="hover" target="div" style="width:70px;height:70px">
-                            </lord-icon>
-                            <h5 class="fs-16 mt-3">PRINCIPIOS</h5>
-                            <p class="text-muted">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                            <div>
-                                <a href="#!" class="link-effect link-primary">Read More <i class="bi bi-arrow-right ms-2"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <section class="ecommerce-about-team bg-light bg-opacity-50">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-6">
-                    <div class="text-center mb-5">
-                        <h2 class="mb-3">Expart Management Team</h2>
-                        <P class="text-muted fs-15">An Expert Leader is first and foremost someone who has deep expertise in the area that they are leading.</P>
-                    </div>
-                </div>
-            </div>
-            <div class="row gy-4">
-            <div class="col-xl-3 col-md-6">
-                    <div class="team-box text-center">
-                        <div class="team-img">
-                            <img src="front/images/users/user-dummy-img.jpg" alt="" class="img-fluid rounded rounded-circle border border-dashed border-dark border-opacity-25">
-                        </div>
-                        <div class="mt-4 pt-1">
-                            <a href="#!">
-                                <h5>Founder</h5>
-                            </a>
-                            <p class="text-muted mb-0">Founder position</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-6">
-                    <div class="team-box text-center">
-                        <div class="team-img">
-                            <img src="front/images/users/user-dummy-img.jpg" alt="" class="img-fluid rounded rounded-circle border border-dashed border-dark border-opacity-25">
-                        </div>
-                        <div class="mt-4 pt-1">
-                            <a href="#!">
-                                <h5>Founder</h5>
-                            </a>
-                            <p class="text-muted mb-0">Founder position</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-6">
-                    <div class="team-box text-center">
-                        <div class="team-img">
-                            <img src="front/images/users/user-dummy-img.jpg" alt="" class="img-fluid rounded rounded-circle border border-dashed border-dark border-opacity-25">
-                        </div>
-                        <div class="mt-4 pt-1">
-                            <a href="#!">
-                                <h5>Co-founder</h5>
-                            </a>
-                            <p class="text-muted mb-0">Co-founder</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-3 col-md-6">
-                    <div class="team-box text-center">
-                        <div class="team-img">
-                            <img src="front/images/users/user-dummy-img.jpg" alt="" class="img-fluid rounded rounded-circle border border-dashed border-dark border-opacity-25">
-                        </div>
-                        <div class="mt-4 pt-1">
-                            <a href="#!">
-                                <h5>Co-founder</h5>
-                            </a>
-                            <p class="text-muted mb-0">Co-founder</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="section">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-lg-6">
-                    <div>
-                        <img src="front/images/ecommerce/img-5.jpg" alt="" class="img-fluid rounded">
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="mt-4 mt-lg-0">
-                        <p class="text-uppercase fw-medium text-muted">Title</p>
-                        <h2 class="lh-base fw-semibold mb-3">Example title</h2>
-                        <P class="text-muted fs-16">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</P>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+        </section>
 
 @stop
